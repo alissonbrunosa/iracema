54e737d12aa841a697c39da4ee4cdf9312827087
Alisson Santos <alissonbruno.sa@gmail.com>
2022-03-20 19:43:48 +0100

    Changes:
    Make method a IrObject add *base to it.
    
    Add new constructor to create an IrMethod.
    its body will be an array of uint16.

--- lang/method.go
+++ lang/method.go
@@ -8,16 +8,23 @@ const (
 )
 
 type Method struct {
+	*base
+
 	methodType MethodType
 	name       string
 	arity      byte
 	body       interface{}
+	localCount byte
+	constants  []IrObject
 }
 
 func (m *Method) Name() string           { return m.name }
 func (m *Method) Arity() byte            { return m.arity }
 func (m *Method) Body() interface{}      { return m.body }
+func (m *Method) Instrs() []uint16       { return m.body.([]uint16) }
 func (m *Method) MethodType() MethodType { return m.methodType }
+func (m *Method) Constants() []IrObject  { return m.constants }
+func (m *Method) LocalCout() byte        { return m.localCount }
 
 func NewGoMethod(name string, body Native, arity byte) *Method {
 	return &Method{
@@ -25,5 +32,16 @@ func NewGoMethod(name string, body Native, arity byte) *Method {
 		name:       name,
 		arity:      arity,
 		body:       body,
+	}
+}
+
+func NewIrMethod(name string, arity byte, body []uint16, localCount byte, consts []IrObject) *Method {
+	return &Method{
+		methodType: IrMethod,
+		name:       name,
+		arity:      arity,
+		body:       body,
+		localCount: localCount,
+		constants:  consts,
 	}
 }
