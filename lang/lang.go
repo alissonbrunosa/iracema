363904e62d180997d77af63d65adb7aca9f44682
Alisson Santos <alissonbruno.sa@gmail.com>
2022-03-20 23:27:15 +0100

    Changes:
    Load all core class in the init function.
    Define the Runtime interface.
    
    Some utility methods.

--- lang/lang.go
+++ lang/lang.go
@@ -1,7 +1,65 @@
 package lang
 
+var classes map[string]*Class
+
+func init() {
+	InitClass()
+	InitObject()
+	InitError()
+	InitString()
+	InitFloat()
+	InitInt()
+	InitNone()
+	InitBool()
+	InitHash()
+	InitArray()
+
+	classes = map[string]*Class{
+		"Object":  ObjectClass,
+		"Int":     IntClass,
+		"Float":   FloatClass,
+		"String":  StringClass,
+		"None":    NoneClass,
+		"Boolean": BoolClass,
+		"Hash":    HashClass,
+		"Array":   ArrayClass,
+
+		"Error":             Error,
+		"RuntimeError":      RuntimeError,
+		"ArgumentError":     ArgumentError,
+		"NoMethodError":     NoMethodError,
+		"ZeroDivisionError": ZeroDivisionError,
+	}
+}
+
+func IsTruthy(obj IrObject) Bool {
+	if obj.Class() == NoneClass {
+		return false
+	}
+
+	if obj.Class() != BoolClass {
+		return true
+	}
+
+	return BOOL(obj)
+}
+
+func TypeLookup(name IrObject) *Class {
+	n := unwrapString(name)
+	return classes[string(n)]
+}
+
+func DefineType(name string, class *Class) {
+	classes[name] = class
+}
+
 type IrObject interface {
 	Class() *Class
 	Is(*Class) bool
 	LookupMethod(methodName string) *Method
+}
+
+type Runtime interface {
+	SetError(*ErrorObject)
+	Call(IrObject, *Method, ...IrObject) IrObject
 }
