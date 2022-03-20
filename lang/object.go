3104b1bf98fa8f76f81a5cda1416f7c8351bbf6c
Alisson Santos <alissonbruno.sa@gmail.com>
2022-03-20 23:20:19 +0100

    Changes:
    Update object method with the Runtime.
    
    Add allocator function to Object
    The other classes will inherit this func.

--- lang/object.go
+++ lang/object.go
@@ -5,39 +5,44 @@ import (
 	"reflect"
 )
 
-func returnFalse(self IrObject) IrObject {
+func returnFalse(rt Runtime, self IrObject) IrObject {
 	return False
 }
 
-func objectId(self IrObject) IrObject {
+func objectPuts(rt Runtime, self IrObject, args ...IrObject) IrObject {
+	for _, arg := range args {
+		inspect := call(rt, arg, "inspect")
+		if inspect == nil {
+			return nil
+		}
+
+		fmt.Println(inspect)
+	}
+
+	return None
+}
+
+func objectId(rt Runtime, self IrObject) IrObject {
 	ptr := reflect.ValueOf(self).Pointer()
 	return Int(ptr)
 }
 
-func objectEqual(self IrObject, rhs IrObject) IrObject {
+func objectEqual(rt Runtime, self IrObject, rhs IrObject) IrObject {
 	return Bool(self == rhs)
 }
 
-func objectInspect(self IrObject) IrObject {
-	id := objectId(self)
+func objectInspect(rt Runtime, self IrObject) IrObject {
+	id := objectId(rt, self)
 	str := fmt.Sprintf("#<%s:0x%x>", self.Class(), INT(id))
 
 	return NewString(str)
 }
 
-var ObjectClass *Class
-
-func InitObject() {
-	if ObjectClass != nil {
-		return
+func objectAlloc(class *Class) IrObject {
+	return &Object{
+		base:  &base{class: class},
+		attrs: make(map[string]IrObject, 3),
 	}
-
-	ObjectClass = NewClass("Object", nil)
-	ObjectClass.AddGoMethod("==", oneArg(objectEqual))
-	ObjectClass.AddGoMethod("hash", zeroArgs(objectId))
-	ObjectClass.AddGoMethod("object_id", zeroArgs(objectId))
-	ObjectClass.AddGoMethod("inspect", zeroArgs(objectInspect))
-	ObjectClass.AddGoMethod("nil?", zeroArgs(returnFalse))
 }
 
 type Object struct {
@@ -56,4 +61,27 @@ func (o *Object) Get(name string) IrObject {
 
 func (o *Object) String() string {
 	return "<Object:" + o.class.Name() + ">"
+}
+
+func NewObject() *Object {
+	return &Object{
+		base: &base{class: ObjectClass},
+	}
+}
+
+var ObjectClass *Class
+
+func InitObject() {
+	if ObjectClass != nil {
+		return
+	}
+
+	ObjectClass = NewClass("Object", nil)
+	ObjectClass.allocator = objectAlloc
+	ObjectClass.AddGoMethod("==", oneArg(objectEqual))
+	ObjectClass.AddGoMethod("hash", zeroArgs(objectId))
+	ObjectClass.AddGoMethod("puts", nArgs(objectPuts))
+	ObjectClass.AddGoMethod("object_id", zeroArgs(objectId))
+	ObjectClass.AddGoMethod("inspect", zeroArgs(objectInspect))
+	ObjectClass.AddGoMethod("nil?", zeroArgs(returnFalse))
 }
