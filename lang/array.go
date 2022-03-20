cb568eea5e0214b4e0102c8313d3882fbdb0425a
Alisson Santos <alissonbruno.sa@gmail.com>
2022-03-20 23:23:14 +0100

    Changes:
    Update Array class to use the passed runtime.
    Add some new methods.
    - uniq
    - hash
    - (-) (a.k.a minus)
    - (==) (a.k.a equal)
    - inspect
    
    Set error to runtime instead of panic.

--- lang/array.go
+++ lang/array.go
@@ -1,55 +1,105 @@
 package lang
 
-func checkBoundaries(index int, size int) int {
+import (
+	"fmt"
+	"strings"
+)
+
+func toArray(value IrObject) (*Array, *ErrorObject) {
+	if array, ok := value.(*Array); ok {
+		return array, nil
+	}
+
+	var mesg = new(strings.Builder)
+	fmt.Fprintf(mesg, "TypeError: no implicit conversion of %s into Array", value.Class())
+	return nil, NewError(mesg.String(), RuntimeError)
+}
+
+func checkBoundaries(index int, size int) (int, *ErrorObject) {
 	if index < 0 {
 		index += size
 	}
 
 	if index < 0 || index > size-1 {
-		panic("IndexOutOfBoundsException")
+		return 0, NewError("IndexOutOfBoundsException", RuntimeError)
 	}
 
-	return index
+	return index, nil
 }
 
 func ARRAY(obj IrObject) *Array {
 	return obj.(*Array)
 }
 
-func arrayInsert(self, index, element IrObject) IrObject {
+func arrayInspect(rt Runtime, self IrObject) IrObject {
+	array := ARRAY(self)
+
+	if len(array.Elements) == 0 {
+		return NewString("[]")
+	}
+
+	var inspect IrObject
+	var buf strings.Builder
+
+	buf.WriteByte('[')
+	for i, el := range array.Elements {
+		inspect = call(rt, el, "inspect")
+		if i > 0 {
+			buf.WriteString(", ")
+		}
+
+		buf.Write(unwrapString(inspect))
+	}
+	buf.WriteByte(']')
+
+	return NewString(buf.String())
+}
+
+func arrayInsert(rt Runtime, self, index, element IrObject) IrObject {
 	array := ARRAY(self)
 
 	idx := INT(index)
 	size := len(array.Elements)
 
-	pos := checkBoundaries(int(idx), size)
+	pos, err := checkBoundaries(int(idx), size)
+	if err != nil {
+		rt.SetError(err)
+		return nil
+	}
 
 	array.Elements[pos] = element
 	return element
 }
 
-func arrayAt(self IrObject, index IrObject) IrObject {
+func arrayAt(rt Runtime, self IrObject, index IrObject) IrObject {
 	array := ARRAY(self)
+
 	idx := INT(index)
 	size := len(array.Elements)
-	pos := checkBoundaries(int(idx), size)
+
+	pos, err := checkBoundaries(int(idx), size)
+	if err != nil {
+		rt.SetError(err)
+		return nil
+	}
+
 	return array.Elements[pos]
 }
 
-func arrayPush(self IrObject, elements ...IrObject) IrObject {
+func arrayPush(rt Runtime, self IrObject, elements ...IrObject) IrObject {
 	array := ARRAY(self)
 	array.Elements = append(array.Elements, elements...)
 	return array
 }
 
-func arrayFlatten(self IrObject) IrObject {
+func arrayFlatten(rt Runtime, self IrObject) IrObject {
 	array := ARRAY(self)
 
 	var result []IrObject
 	for _, element := range array.Elements {
 		switch element.(type) {
 		case *Array:
-			ary := ARRAY(arrayFlatten(element))
+			ary := ARRAY(arrayFlatten(rt, element))
 			result = append(result, ary.Elements...)
 		default:
 			result = append(result, element)
@@ -59,7 +109,7 @@ func arrayFlatten(self IrObject) IrObject {
 	return NewArray(result)
 }
 
-func arrayReverse(self IrObject) IrObject {
+func arrayReverse(rt Runtime, self IrObject) IrObject {
 	array := ARRAY(self)
 	length := len(array.Elements)
 	elements := make([]IrObject, length)
@@ -71,13 +121,13 @@ func arrayReverse(self IrObject) IrObject {
 	return NewArray(elements)
 }
 
-func arrayLength(self IrObject) IrObject {
+func arrayLength(rt Runtime, self IrObject) IrObject {
 	array := ARRAY(self)
 	length := len(array.Elements)
 	return Int(length)
 }
 
-func arrayValuesAt(self IrObject, indices ...IrObject) IrObject {
+func arrayValuesAt(rt Runtime, self IrObject, indices ...IrObject) IrObject {
 	array := ARRAY(self)
 	elements := make([]IrObject, len(indices))
 
@@ -88,12 +138,43 @@ func arrayValuesAt(self IrObject, indices ...IrObject) IrObject {
 	return NewArray(elements)
 }
 
-func arrayShift(self IrObject, size IrObject) IrObject {
+func arrayHash(rt Runtime, self IrObject) IrObject {
+	array := ARRAY(self)
+
+	hash := Int(1)
+	for _, el := range array.Elements {
+		code := call(rt, el, "hash")
+		hash = hash*31 + INT(code)
+	}
+
+	return hash
+}
+func arrayUniq(rt Runtime, self IrObject) IrObject {
 	ary := ARRAY(self)
-	n := INT(size)
+	if len(ary.Elements) <= 1 {
+		newEls := make([]IrObject, len(ary.Elements))
+		copy(newEls, ary.Elements)
+		return NewArray(newEls)
+	}
+
+	hash := NewHash()
+	for _, el := range ary.Elements {
+		hashInsert(rt, hash, el, el)
+	}
+
+	return hashValues(rt, hash)
+}
+
+func arrayShift(rt Runtime, self IrObject, size IrObject) IrObject {
+	ary := ARRAY(self)
+	n, err := toInt(size)
+	if err != nil {
+		return nil
+	}
 
 	if n < 0 {
-		panic("negative array size")
+		err := NewError("negative array size", ArgumentError)
+		rt.SetError(err)
 		return nil
 	}
 
@@ -119,12 +200,67 @@ func arrayShift(self IrObject, size IrObject) IrObject {
 	return NewArray(result)
 }
 
-func arrayPlus(self, other IrObject) IrObject {
+func arrayEqual(rt Runtime, self IrObject, other IrObject) IrObject {
+	if self == other {
+		return True
+	}
+
+	y, err := toArray(other)
+	if err != nil {
+		rt.SetError(err)
+		return nil
+	}
+
 	x := ARRAY(self)
-	y := ARRAY(other)
+	if len(x.Elements) != len(y.Elements) {
+		return False
+	}
+
+	for i, el := range x.Elements {
+		ret := call(rt, el, "==", y.Elements[i])
+		if !BOOL(ret) {
+			return False
+		}
+	}
+
+	return True
+}
+
+func arrayPlus(rt Runtime, self, other IrObject) IrObject {
+	y, err := toArray(other)
+	if err != nil {
+		rt.SetError(err)
+		return nil
+	}
+
+	x := ARRAY(self)
 	result := make([]IrObject, len(x.Elements)+len(y.Elements))
 	i := copy(result, x.Elements)
 	copy(result[i:], y.Elements)
+
+	return NewArray(result)
+}
+
+func arrayMinus(rt Runtime, self, other IrObject) IrObject {
+	y, err := toArray(other)
+	if err != nil {
+		rt.SetError(err)
+		return nil
+	}
+
+	h := NewHash()
+	for _, el := range y.Elements {
+		hashInsert(rt, h, el, True)
+	}
+
+	x := ARRAY(self)
+	var result []IrObject
+	for _, el := range x.Elements {
+		if BOOL(hashHasKey(rt, h, el)) {
+			continue
+		}
+		result = append(result, el)
+	}
 
 	return NewArray(result)
 }
@@ -137,16 +273,21 @@ func InitArray() {
 	}
 
 	ArrayClass = NewClass("Array", ObjectClass)
+	ArrayClass.AddGoMethod("==", oneArg(arrayEqual))
+	ArrayClass.AddGoMethod("hash", zeroArgs(arrayHash))
 	ArrayClass.AddGoMethod("at", oneArg(arrayAt))
 	ArrayClass.AddGoMethod("get", oneArg(arrayAt))
 	ArrayClass.AddGoMethod("+", oneArg(arrayPlus))
+	ArrayClass.AddGoMethod("-", oneArg(arrayMinus))
 	ArrayClass.AddGoMethod("values_at", nArgs(arrayValuesAt))
 	ArrayClass.AddGoMethod("push", nArgs(arrayPush))
 	ArrayClass.AddGoMethod("insert", twoArgs(arrayInsert))
 	ArrayClass.AddGoMethod("reverse", zeroArgs(arrayReverse))
 	ArrayClass.AddGoMethod("length", zeroArgs(arrayLength))
 	ArrayClass.AddGoMethod("size", zeroArgs(arrayLength))
+	ArrayClass.AddGoMethod("inspect", zeroArgs(arrayInspect))
 	ArrayClass.AddGoMethod("flatten", zeroArgs(arrayFlatten))
+	ArrayClass.AddGoMethod("uniq", zeroArgs(arrayUniq))
 	ArrayClass.AddGoMethod("shift", oneArg(arrayShift))
 }
 
